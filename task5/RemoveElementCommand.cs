b0c29aeaea476a7d6eaf006e2bd2180c73806ba5
Khomenko Nadiia <n.homenko395@gmail.com>
2025-05-08 00:56:34 +0300

    Add RemoveElementCommand to remove elements

---
+++ task5/RemoveElementCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    public class RemoveElementCommand : ICommand
+    {
+        private readonly LightElementNode parent;
+        private readonly LightElementNode child;
+
+        public RemoveElementCommand(LightElementNode parent, LightElementNode child)
+        {
+            this.parent = parent;
+            this.child = child;
+        }
+
+        public void Execute()
+        {
+            parent.RemoveChild(child);
+        }
+
+        public void Undo()
+        {
+            parent.AddChild(child);
+        }
+    }
+}
