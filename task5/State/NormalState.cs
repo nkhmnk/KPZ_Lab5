6a0f8f2effad2cb8f6fe13dfef38cfaccd679262
Khomenko Nadiia <n.homenko395@gmail.com>
2025-05-12 16:14:46 +0300

    implemented concrete states: NormalState, HoveredState, SelectedState

---
+++ task5/State/NormalState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5.State
+{
+    public class NormalState : IElementState //Нормальний стан
+    {
+        public void AddClass(LightElementNode element, string cssClass)
+        {
+            element.AddCssClass(cssClass); // Додаємо клас до елемента
+        }
+
+        public void RemoveClass(LightElementNode element, string cssClass)
+        {
+            element.RemoveCssClass(cssClass); // Видаляємо клас з елемента
+        }
+    }
+
+}
