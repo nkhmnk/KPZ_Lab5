0011661ac62c6f4edf9cb07e4c85cc1cd3355257
Khomenko Nadiia <n.homenko395@gmail.com>
2025-05-12 17:50:31 +0300

    added AddAttributeVisitor for adding attributes to elements

---
+++ task5/Visitor/AddAttributeVisitor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5.Visitor
+{
+    public class AddAttributeVisitor : IElementVisitor
+    {
+        private readonly string _attributeName;
+        private readonly string _attributeValue;
+
+        public AddAttributeVisitor(string attributeName, string attributeValue)
+        {
+            _attributeName = attributeName;
+            _attributeValue = attributeValue;
+        }
+
+        public void Visit(LightElementNode element)
+        {
+            // Додаємо атрибут до елемента
+            element.AddCssClass($"{_attributeName}=\"{_attributeValue}\"");
+        }
+
+        public void Visit(LightTextNode textNode)
+        {
+        }
+    }
+
+}
